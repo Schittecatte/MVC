8add735f198ee5db7fcb43fbbbef9c3810736f31
Niels Schittecatte <staict01@renson.be>
2018-02-18 18:12:39 +0100

    First commit

---
+++ Main/Models/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Main.Models
+{
+    public class Main
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+    public class MainDBContext : DbContext
+    {
+        public DbSet<Main> Mains { get; set; }
+    }
+}
